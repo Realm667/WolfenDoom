88c268f2d4759a2843c53c18ffa181a4a9f38e4f
Ozymandias81 <Ozymandias81@gmx.com>
2016-08-08 13:47:24 +0200

    Small changes
    BATCH:
    :new: Excluded from compression *.bat, *.backup* and *.dbs files (these
    last two only from Maps folder)
    :new: Changed the compression method from LZMA 9 to Deflate 0, now it's
    just storing files into a .pk3
    
    MAPS - C1M0:
    :new: Removed the double Player 1 Start

--- _zip.bat
+++ _zip.bat
@@ -1,2 +1,2 @@
 @echo off
-tools\7za a -tzip -mm=LZMA -mx=9 -x!".git*" ..\wolf_boa.pk3 *
+tools\7za a -tzip -mm=Deflate -mx=0 -x!".git*" -x!"*.bat*" -x!"maps\*.backup*" -x!"maps\*.dbs*" ..\wolf_boa.pk3 *
