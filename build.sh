ac8a4a6dc86ceeac4bb386aee6ed5657137a160e
Kevin Caccamo <kevin@ciinet.org>
2016-08-10 19:20:38 -0400

    Use the exclusion lists in build.sh.
    Rename _zip.bat to build.bat for consistency.
    Use the exclusion lists in build.sh.
    Change the way 7zExcludeList.txt is used in build.bat.
    Change the flag for build.sh to use LZMA or Deflate.
    Make the filename/path cases in 7zExcludeList.txt and 7zExcludeListDir.txt match the actual cases of the directories and files. Unix is case sensitive.

--- build.sh
+++ build.sh
@@ -6,15 +6,15 @@ git pull origin master
 zipprefix="wolf_boa"
 zipname="$zipprefix-$(git log -n 1 --date=short --format=%h-%cd | sed 's/\([[:digit:]]\{4\}\)-\([[:digit:]]\{2\}\)-\([[:digit:]]\{1,2\}\)$/\1\2\3/').pk3"
 
-if [[ "$1" == '-dev' ]]; then
-  cmthd='Deflate'
+if [[ "$1" == "-r" || "$1" == "--release" ]]; then
+  cmthd='LZMA'
   shift
 else
-  cmthd='LZMA'
+  cmthd='Deflate'
 fi
 
 # print -l **/*~*.bat~*.exe~.*~*.bak~source/*(#q.on) | zip -@ $zipname
-7z a -tzip -mmt=on -mm=$cmthd -mx=9 -ssc -x!'.git*' -x!'**/*.bak' -x!'*.bat' -x!'**/*.exe' -x!'**/*.acs' $zipname *
+noglob 7z a -tzip -mmt=on -mm=$cmthd -mx=9 -ssc -x@'tools/7zExcludeList.txt' -x@'tools/7zExcludeListDir.txt' $zipname *
 
 mv $zipname ..
 
