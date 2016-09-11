db60faeba003a1a4ebb35e94d0a9c3411aac1a9b
Nicholas "Tiger" Gautier <Nicholas.Gautier.Tiger@GMail.com>
2016-09-11 17:10:57 -0500

    Bug fix: Tested a bool with a string....
    I used an incorrect variable when trying to reduce some redundancy.

--- build.bat
+++ build.bat
@@ -261,7 +261,7 @@ REM If git features is not available, then just use the generic name.
 REM  No hash will be used.
 IF %featuresGit% NEQ True (
     REM Avoid redundancy
-    IF "%featuresGit%" NEQ "wolf_boa" SET "projectName=wolf_boa"
+    IF "%projectName%" NEQ "wolf_boa" SET "projectName=wolf_boa"
     GOTO :EOF
 )
 REM Assume Git features are available for us to utilize
