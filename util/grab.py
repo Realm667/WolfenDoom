98f18928e6e85471d9643c3fd5903fcad25bb410
Kevin Caccamo <kevin@ciinet.org>
2020-06-21 10:00:21 -0400

    Add more capital Latin characters with accents
    Also, add some comments, a hashbang line, and change the mode of grab.py

--- util/grab.py
+++ util/grab.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # Output a grAb chunk for copy/pasting into a PNG
 
 import struct
@@ -5,15 +6,17 @@ import zlib
 import argparse
 import sys
 
+# Offsets as arguments
 parser = argparse.ArgumentParser(description="Output a grAb chunk for PNGs")
 parser.add_argument('x', type=int, help="x offset")
 parser.add_argument('y', type=int, help="y offset")
-
 offsets = parser.parse_args()
 
+# Convert to binary
 grabs = struct.pack(">ii", offsets.x, offsets.y)
 crc = struct.pack(">I", zlib.crc32(grabs, 0))
 size = struct.pack(">I", 8)
 output = size + b"grAb" + grabs + crc
 
+# Write to stdout
 sys.stdout.buffer.write(output)
