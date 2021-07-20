0bcd3db0b3ed809457e9ed73cfae701475780f2c
Kevin Caccamo <kevin@ciinet.org>
2021-07-19 21:57:02 -0400

    Add new tools for automatically cropping fonts
    Rewrite grab.py to be more useful as a Python module
    Add grab_inject.py, which can generate a grAb chunk and inject it directly into a PNG file
    Add fontcut.sh, which uses Imagemagick to automatically remove blank rows above/below font character images

---
+++ tools/programs/fontcut.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env zsh
+zmodload zsh/pcre
+
+fontdir=${1:?Please specify a font directory}
+
+process_char(){
+    fontchar=$1
+    yoffset=${2:=0}
+    baseheight=${3:=0}
+    pcre_compile -im '\s(\d+)x(\d+)\s'
+    charinfo="$(identify $fontchar 2>/dev/null)" || continue
+    pcre_match -a chardims $charinfo
+    charwidth=${chardims[1]}
+    channels=3
+    hexdigitsperoctet=2
+    charrowstride=$((charwidth * channels * hexdigitsperoctet))
+    sedscript="s/([[:alnum:]]{${charrowstride}})/\1\n/g"
+    : ${(Af)chardata::="$(convert $fontchar -depth 8 rgb:- | \
+        xxd -p -g 0 | tr -d '\n' | sed -E $sedscript)"}
+    blankrow=""
+    repeat $charrowstride blankrow+="0"
+    cuttoprows=0
+    cutbtmrows=0
+    lasttoprow=0
+    lastbtmrow=0
+    for ((rownum=1; rownum <= ${#chardata}; rownum++)) {
+        rowdata=${chardata[$rownum]}
+        if [[$rowdata == $blankrow]]; then
+            ((cuttoprows += 1))
+        else
+            lasttoprow=$rownum
+            break
+        fi
+    }
+    for ((rownum=${#chardata}; rownum > 0; rownum--)) {
+        rowdata=${chardata[$rownum]}
+        if [[$rowdata == $blankrow]]; then
+            ((cutbtmrows += 1))
+        else
+            lastbtmrow=$rownum
+            break
+        fi
+    }
+    charheight=$((lastbtmrow - lasttoprow))
+    offsety=$(( yoffset - (charheight - baseheight) ))
+    python3 grab_inject.py $fontchar 0 $offsety
+}
+
+for fontchar in ${fontdir}/*(.); do
+    process_char $fontchar $2 $3
+done
--- tools/programs/grab.py
+++ tools/programs/grab.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
+"""
+grab.py - A module for working with grAb chunks in ZDoom PNGs
+
+Functions:
+get_grabs - given an X and Y offset, get the grAb chunk bytes
+"""
 # Output a grAb chunk for copy/pasting into a PNG
 
 import struct
@@ -6,26 +12,48 @@ import zlib
 import argparse
 import sys
 
-# Offsets as arguments
-parser = argparse.ArgumentParser(description="Output a grAb chunk that can be copied and pasted into PNG files.")
-parser.add_argument('x', type=int, help="x offset")
-parser.add_argument('y', type=int, help="y offset")
-parser.add_argument('--bad', help="Use bad CRC calculation", action='store_true')
-offsets = parser.parse_args()
 
-# Convert to binary
-if offsets.bad:
-    grabs = struct.pack("!ii", offsets.x, offsets.y)
-else:
-    grabs = b"grAb" + struct.pack("!ii", offsets.x, offsets.y)
+def get_bytes(x=0, y=0):
+    """
+    Get the PNG grAb chunk bytes using the given x/y offsets
+    x - The X (horizontal) offset
+    y - The Y (vertical) offset
+    """
+    return struct.pack("!ii", x, y)
 
-crc = struct.pack("!I", zlib.crc32(grabs))
-size = struct.pack("!i", 8)
 
-if offsets.bad:
-    output = size + b"grAb" + grabs + crc
-else:
-    output = size + grabs + crc
+def get_grabs(x=0, y=0, bad=False):
+    """
+    Get the PNG grAb chunk data using the given x/y offsets
+    x - The X (horizontal) offset
+    y - The Y (vertical) offset
+    bad - Whether or not to calculate the chunk's CRC incorrectly
+    """
+    # Convert to binary
+    if bad:
+        grabs = get_bytes(x, y)
+    else:
+        grabs = b"grAb" + get_bytes(x, y)
 
-# Write to stdout
-sys.stdout.buffer.write(output)
+    crc = struct.pack("!I", zlib.crc32(grabs))
+    size = struct.pack("!i", 8)
+
+    if bad:
+        return size + b"grAb" + grabs + crc
+    return size + grabs + crc
+
+
+if __name__ == "__main__":
+    # Offsets as arguments
+    parser = argparse.ArgumentParser(
+        description="Output a grAb chunk that can be copied and pasted into "
+                    "PNG files.")
+    parser.add_argument('x', type=int, help="x offset")
+    parser.add_argument('y', type=int, help="y offset")
+    parser.add_argument('--bad', help="Use bad CRC calculation",
+                        action='store_true')
+    offsets = parser.parse_args()
+    output = get_grabs(**vars(offsets))
+
+    # Write to stdout
+    sys.stdout.buffer.write(output)
